c5d7d16ee70a858ecb6a0a21d42b7b9327c3efdb
Ashemarc <152915640+Ashenafi-Abebe@users.noreply.github.com>
2024-03-28 22:16:54 -0700

    footer, postqueation and answer pages finalized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,66 @@
-import { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState, createContext } from "react";
+import Landing from "./pages/LandingLayout/LandingLayout";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import axios from "./axiosConfig";
+import AskQuestion from "./components/AskQuestion/AskQuestion";
+import Answer from "./components/Answer/Answer";
+// import Footer from "./components/Footer/Footer";
 
 export const AppState = createContext();
 
 function App() {
   const [user, setuser] = useState({});
+  const [question, setQuestion] = useState({});
+
+  // console.log(question);
+  console.log(user);
 
   const token = localStorage.getItem("token");
+  // console.log(token)
   const navigate = useNavigate();
+
   async function checkUser() {
     try {
       const { data } = await axios.get("/users/check", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setuser(data);
+      // console.log(data)
+    } catch (error) {
+      navigate("/Login");
+      console.log(error.response);
+    }
+  }
+
+  async function getQuestion() {
+    try {
+      const { data } = await axios.get("/questions/all-questions", {
         headers: {
           Authorization: "Bearer " + token,
         },
       });
-      setuser(data);
+      // console.log(data)
+      setQuestion(data); // Assuming data holds the question value
     } catch (error) {
-      console.log(error.respones);
-      navigate("/Login");
+      console.error("Error fetching question:", error);
     }
   }
 
   useEffect(() => {
     checkUser();
+    getQuestion();
   }, []);
 
   return (
-    <AppState.Provider value={{ user, setuser }}>
+    <AppState.Provider value={{ user, setuser, question, setQuestion, token }}>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="Login" element={<Login />}></Route>
-        <Route path="Register" element={<Register />}></Route>
+        <Route path="/Login" element={<Landing />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/questions" element={<AskQuestion />} />
+        <Route path="/answer" element={<Answer />} />
+        {/* <Route path="/Footer" element={<Footer />} /> */}
       </Routes>
     </AppState.Provider>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,74 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import { useNavigate, Link } from "react-router-dom";
+import { PiUserCircleDuotone } from "react-icons/pi";
+import { FaAngleRight } from "react-icons/fa";
 import { AppState } from "../App";
 
-function Home() {
-  const { user } = useContext(AppState);
+const Home = () => {
+  const { user, question } = useContext(AppState);
+
+  const navigate = useNavigate();
+
+  const handleAskQuestionClick = () => {
+    navigate("/questions"); // Navigates to the "/questions" route
+  };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <br />
-      <br />
-      <br />
-      <h2>Wellcome:{user.username}</h2>
-    </div>
+    <>
+      <Header />
+
+      <section className="bg-body-tertiary">
+        <div className="d-flex justify-content-around pt-5">
+          <button
+            onClick={handleAskQuestionClick}
+            className="btn btn-primary action_btn px-5"
+          >
+            Ask Question
+          </button>
+
+          <p className="fw-semibold">
+            <span className="text-warning">Welcome,Ashenafi </span>
+            {user?.username}
+          </p>
+        </div>
+
+        <div className="container mt-5">
+          <h2>Question</h2>
+
+          {question.question?.map((item, index) => (
+            <Link
+              className="text-decoration-none text-black"
+              key={index}
+              to={`/answer?title=${encodeURIComponent(
+                item.title
+              )}&description=${encodeURIComponent(
+                item.description
+              )}&questionid=${encodeURIComponent(item.questionid)}`}
+            >
+              <hr />
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="d-md-flex align-items-center gap-4">
+                  <div className="d-flex flex-column align-items-center gap-3">
+                    <PiUserCircleDuotone size={100} />
+                    <div>{item.username}</div>
+                  </div>
+                  <div>
+                    <p>{item.title}</p>
+                  </div>
+                </div>
+
+                <FaAngleRight size={30} />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+
+      {/* <Footer /> */}
+    </>
   );
-}
+};
 
 export default Home;
